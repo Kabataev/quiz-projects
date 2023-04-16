a298d65d43f3a2ddcbb2974b49de9c9be55dc3bf
Kabataev Arslen <kabataev.92@gmail.com>
2023-04-16 14:08:34 +0600

    Закончена логика счётчика

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+
+  let[count , setCount] = useState(0)
+
+  function handleCountPlus(){
+    setCount(()=>count++);
+  }
+
+  function handleCountMinus(){
+    setCount(()=>{
+      if(count<=1)return count
+      
+      return count--;
+    });
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={handleCountMinus} className="minus">- Минус</button>
+        <button onClick={handleCountPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
